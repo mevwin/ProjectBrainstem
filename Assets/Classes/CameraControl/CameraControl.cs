5fb41569e7c5ac5dec63c75bae6bc04176c0cb85
ColinRafferty7 <ctr6mince@gmail.com>
2026-05-03 00:28:58 -0400

    Fixing camera problems

--- Assets/Classes/CameraControl/CameraControl.cs
+++ Assets/Classes/CameraControl/CameraControl.cs
@@ -5,23 +5,27 @@ public class CameraControl : MonoBehaviour
 {
     public Transform CameraOffsetY;
     public GameObject mainCamera;
-    [SerializeField] private float sensitivity, lowerBound, upperBound;
+    [SerializeField] private float sensitivity, lowerBound, upperBound, distance;
 
     InputAction look;
     Vector2 lookAmt;
 
     float vertRotation = 0;
-    float horizRotation = 0;
+    float horRotation = 0;
 
     void Start()
     {
+        // Get InputAction references from Project-wide input actions.
         if (InputSystem.actions)
+        {
             look = InputSystem.actions.FindAction("Player/Look");
+        }
     }
 
     private void Update()
     {
-        if (Time.timeScale == 1f) {
+        if (Time.timeScale == 1f)
+        {
             lookAmt = look.ReadValue<Vector2>();
             Rotating();
             Positioning();
@@ -33,36 +37,40 @@ public class CameraControl : MonoBehaviour
         if (lookAmt.x != 0f)
         {
             lookAmt.x *= sensitivity;
-            horizRotation += lookAmt.x;
+            horRotation += lookAmt.x;
+
         }
 
         if (lookAmt.y != 0f)
         {
             lookAmt.y *= sensitivity;
-            vertRotation = Mathf.Clamp(vertRotation - lookAmt.y, lowerBound, upperBound);
-        }
 
-        CameraOffsetY.transform.localRotation = Quaternion.Euler(vertRotation, horizRotation, 0);
+            vertRotation = Mathf.Clamp(vertRotation - lookAmt.y, lowerBound, upperBound);
+
+            
+        }
+        CameraOffsetY.transform.localRotation = Quaternion.Euler(vertRotation, horRotation, 0);
     }
 
     private void Positioning()
     {
         if (Physics.Raycast(gameObject.transform.position + new Vector3(0f, 2f, 0f), mainCamera.transform.forward * -1, out RaycastHit hit))
         {
-            if (hit.distance < 6f)
+            if (hit.distance < distance)
             {
-                //Debug.Log(hit.transform.gameObject.name);
-                CameraOffsetY.transform.position = hit.point + mainCamera.transform.forward;
+                CameraOffsetY.transform.position = hit.point + hit.normal * 0.1f;
+                return;
             }
             else
             {
-                CameraOffsetY.transform.position = gameObject.transform.position + mainCamera.transform.forward * -6;
+                CameraOffsetY.transform.position = gameObject.transform.position + mainCamera.transform.forward * -distance;
             }
         }
         else
         {
-            CameraOffsetY.transform.position = gameObject.transform.position + mainCamera.transform.forward * -6;
+            CameraOffsetY.transform.position = gameObject.transform.position + mainCamera.transform.forward * -distance;
         }
         CameraOffsetY.transform.position += new Vector3(0f, 2f, 0f);
     }
+
 }
